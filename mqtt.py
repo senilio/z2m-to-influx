a7e7f0d43ba44c6726f85074d28eb1e19365233c
senilio <git@melt.se>
2021-11-25 08:54:47 +0100

    Don't use the usual main function...

--- mqtt.py
+++ mqtt.py
@@ -10,15 +10,15 @@ attributes = ["power", "temperature", "humidity"]
 #################################################
 
 def log(text):
-    timestamp = '[{:%Y-%m-%d %H:%M:%S.%f}]'.format(datetime.datetime.now())
+    timestamp = '[{:%Y-%m-%d %H:%M:%S}]'.format(datetime.datetime.now())
     print(f'{timestamp} {text}')
 
 def push_to_influx(x):
-    client.write([x],{'db':db_name}, 204, 'line')
+    db.write([x],{'db':db_name}, 204, 'line')
 
 def on_connect(client, userdata, flags, rc):
     log("Connected with result code " + str(rc))
-    client.subscribe("zigbee2mqtt/+")
+    client.subscribe(str(mqtt_topic))
 
 def on_message(client, userdata, msg):
     topic = msg.topic
@@ -29,25 +29,22 @@ def on_message(client, userdata, msg):
             push_to_influx(f'{topic} {attribute}={payload[attribute]}')
 
 
-def main():
-    # First read environment variables provided by docker
-    db_name    = os.environ['INFLUX_DB_NAME']
-    db_host    = os.environ['INFLUX_DB_HOST']
-    db_port    = os.environ['INFLUX_DB_PORT']
-    mqtt_host  = os.environ['MQTT_HOST']
-    mqtt_port  = os.environ['MQTT_PORT']
-    mqtt_topic = os.environ['MQTT_TOPIC']
+# First read environment variables provided by docker
+db_name    = os.environ['INFLUX_DB_NAME']
+db_host    = os.environ['INFLUX_DB_HOST']
+db_port    = os.environ['INFLUX_DB_PORT']
+mqtt_host  = os.environ['MQTT_HOST']
+mqtt_port  = os.environ['MQTT_PORT']
+mqtt_topic = os.environ['MQTT_TOPIC']
 
-    # Initialize connection to Influx
-    client = InfluxDBClient(host=db_host, port=db_port, database=db_name)
+# Initialize connection to Influx
+db = InfluxDBClient(host=db_host, port=db_port, database=db_name)
 
-    # Set up MQTT subscriber
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(mqtt_host, mqtt_port, 60)
+# Set up MQTT subscriber
+mq = mqtt.Client()
+mq.on_connect = on_connect
+mq.on_message = on_message
+mq.connect(mqtt_host, int(mqtt_port), 60)
 
-    client.loop_forever()
-
-if __name__ == '__main__':
-    main()
+# Listen to the subscribed topic forever
+mq.loop_forever()
